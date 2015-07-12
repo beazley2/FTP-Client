129fffb39c2a63ac5bd0af8cba66156cf95caabf
Brad Beazley <beazley2@pdx.edu>
2015-07-12 09:45:10 -0700

    Initial project shell

---
+++ FTP-Client.java
@@ -0,0 +1,21 @@
+package ftp.cs410agile;
+
+/**
+ * Implements a simple FTP client
+ *
+ * @version 0.1
+ */
+public class FTP-Client {
+
+  public static void main(String[] args) {
+    System.exit(0);
+  }
+
+
+}
+
+
+
+
+
+
