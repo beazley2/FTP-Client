fa7d7fec80eaedc9869d988de3476a36810ff729
caesarfish <schottec@gmail.com>
2015-07-27 20:32:32 -0700

    initial commit

---
+++ ftp/cs410agile/FTPClientTest.java
@@ -0,0 +1,14 @@
+package ftp.cs410agile;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Evan on 7/27/2015.
+ */
+public class FTPClientTest {
+
+  @org.junit.Test
+  public void testMain() throws Exception {
+
+  }
+}
